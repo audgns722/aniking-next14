3fcf0e248472c881e14006a2c8b39802044473d6
audgns722 <audgns722@gmail.com>
2024-01-21 18:55:52 +0900

    12

---
+++ app/layout.tsx
@@ -0,0 +1,32 @@
+import type { Metadata } from "next";
+import { DM_Sans } from "next/font/google";
+
+import Hero from "@/components/Hero";
+import Footer from "@/components/Footer";
+
+import "./globals.css";
+
+const dmSans = DM_Sans({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "AniKing",
+  description: "원하는 애니메이션을 만나보실 수 있습니다.",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="ko">
+      <body className={dmSans.className}>
+        <main className="max-w-7xl mx-auto bg-[#0F1117]">
+          <Hero />
+          {children}
+          <Footer />
+        </main>
+      </body>
+    </html>
+  );
+}
